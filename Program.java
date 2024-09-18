cc94974fd6ae221ac85dc7352f13244407e327a1
gamer6700k <62044986+gamer6700k@users.noreply.github.com>
2024-09-18 00:44:55 +0000

    update

--- Program.java
+++ Program.java
@@ -1,7 +1,51 @@
 //import stuff here
+import java.util.Scanner;
 
 //Your code here
 
+public class Program7{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter schrute-bucks: ");
+        
+        int schruteBucksInput = input.nextInt();
+        
+        
+        System.out.println("Enter klevins: ");
+        
+        int klevinsInput= input.nextInt();
+        
+        
+        System.out.println("Enter stanley-nickels: ");
+        
+        int stanleyNickelsInput = input.nextInt();
+        
+        final double schruteBucksConversion = 1.0;
+        final double klevinsConversion = 0.05;
+        final double stanleyNickelsConversion = 0.05 / 12;
+        
+        
+        double schruteBucks = schruteBucksConversion * schruteBucksInput;
+        
+        double klevins = klevinsConversion * klevinsInput;
+        
+        double stanleyNickels = stanleyNickelsConversion * stanleyNickelsInput;
+        
+        schruteBucks = schruteBucks + klevins + stanleyNickels;
+        
+        //Rounding
+        
+        schruteBucks = schruteBucks * 100;
+        
+        schruteBucks = Math.round(schruteBucks);
+        
+        schruteBucks = schruteBucks / 100;
+        
+
+        
+        System.out.println("Decimal schrute-bucks = $" + schruteBucks);
+    }
 //Paste console output below:
 /*
 
